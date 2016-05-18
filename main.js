346b02b57460405e683b21b85e9812b0f97edff7
Ante Borzić <aborzic@extensionengine.com>
2016-05-18 10:30:15 +0200

    finish

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 var http = require('http');
 var map = require('through2-map');
+var strftime = require('strftime');
 
 var port = process.argv[2] || 12345
 
 var server = http.createServer(function(request, response) {
-    request.pipe(map(function(chunk) {
-      return chunk.toString().toUpperCase();
-    })).pipe(response);
+  if (request.method != 'POST') {
+    return response.end('a POST dude');
+  }
+
+  request.pipe(map(function(chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(response);
 });
 
 server
   .on('listening', function() {
     console.log('Server started on port ' + port);
+  })
+  .on('connection', function() {
+    console.log('New connection! ' + strftime('%Y-%m-%d %H:%M'));
   })
   .listen(port);
